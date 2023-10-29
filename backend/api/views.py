948794573c645372ffd6a2c7960f51a205f54bc5
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-10-28 22:23:31 -0300

    feat(api): add home view endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def api_home(request):
+    return JsonResponse({
+        "message": "Hi there, this is your Django API response"
+    })
